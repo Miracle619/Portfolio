bef623516851fe6a04e692bbca36f34a9a7df0f2
Dipesh <merchant.dipesh@gmail.com>
2021-02-07 19:37:17 +0530

    My first Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import Particles from 'react-particles-js';
+import '../Components/Header.css'
+import { Wave, Random } from 'react-animated-text';
+
+class Header extends Component {
+  render() {
+    if (this.props.data) {
+      var name = this.props.data.name;
+      var occupation = this.props.data.occupation;
+      var description = this.props.data.description;
+      var city = this.props.data.address.city;
+      var networks = this.props.data.social.map(function (network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url}>
+              <i className={network.className}></i>
+            </a>
+          </li>
+        );
+      });
+    }
+     
+    return (
+      <header id="home">
+
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
+
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Works
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#testimonials">
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <Particles className="particle--intro" params = {{
+          particles: { 
+              number: { 
+                value: 150, 
+                density: { 
+                  enable: true, 
+                  value_area: 1000, 
+                } 
+              },
+              move: {
+                speed: 2.0
+              },
+             
+            }, }} />
+        <div className="row banner">
+          <div className="banner-text">
+            <h1><Wave text="DIPESH MERCHANT" effect="jump"  effectChange={2.0} /></h1>
+            
+            <h3>
+              I'm a {city} based <span>{occupation}</span>. {description}.
+            </h3>
+            <hr />
+            <ul className="social">{networks}</ul>
+          </div>
+        </div>
+
+        {/* <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p> */}
+      </header>
+    );
+  }
+}
+
+export default Header;
